0317f2e83a9f5ff9a9e8069bde08466be7137fb9
Thomas Steven <thomaspatricksteven@gmail.com>
2020-10-04 20:24:04 -0400

    Basic working version

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
 
 	"gopkg.in/yaml.v2"
 )
@@ -16,40 +20,113 @@ type Book struct {
 type Page struct {
 	PageNumber int      `yaml:"pageNumber"`
 	Text       string   `yaml:"text"`
-	Choices    []Choice `yaml:"choices"`
+	Choices    []string `yaml:"choices"`
 }
 
-type Choice struct {
-	PageNumber int    `yaml:"pageNumber"`
-	Text       string `yaml:"text"`
-}
+const firstPageNumber = 1
+const separator = "~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
 func main() {
-	book, err := getBook()
-	if err != nil {
-		fmt.Println("There was an error ", err)
+	if len(os.Args) != 2 {
+		fmt.Println("You must pass a single argument, the filename of the story (ex: story.yaml)")
+		os.Exit(1)
 	}
 
-	readPage(book, 1)
+	book, err := getBook()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	readBook(book)
 }
 
-func readPage(book *Book, pageNumber int) (nextPage int) {
-	fmt.Println("Page ", pageNumber)
+func readBook(book *Book) {
+	fmt.Println()
+	fmt.Println(book.Title)
+	fmt.Println()
+	fmt.Println("by")
+	fmt.Println()
+	fmt.Println(book.Author)
+
+	page, _ := getPage(book, firstPageNumber)
+
+	for {
+		printPage(page)
+
+		if isTerminalPage(page) {
+			break
+		}
+
+		page = chooseNextPage(book)
+	}
+}
+
+func getPage(book *Book, pageNumber int) (Page, error) {
+	var matchingPage Page
 	for _, page := range book.Pages {
 		if page.PageNumber == pageNumber {
-			println(page.Text)
-			for _, choice := range page.Choices {
-				println(choice.Text)
-			}
+			matchingPage = page
+			return matchingPage, nil
 		}
 	}
 
-	return 0
+	return matchingPage, fmt.Errorf("There is no page %v in this book.", pageNumber)
+}
+
+func chooseNextPage(book *Book) Page {
+	for {
+		fmt.Print("\n", "Choose a page: ")
+		reader := bufio.NewReader(os.Stdin)
+		userInput, _ := reader.ReadString('\n')
+		userInput = strings.TrimSuffix(userInput, "\n")
+		pageNumber, err := strconv.Atoi(userInput)
+
+		if err != nil {
+			fmt.Printf("\n\"%v\" is not a valid input.\n\n", userInput)
+			fmt.Println(separator)
+			continue
+		}
+
+		page, err := getPage(book, pageNumber)
+
+		if err != nil {
+			fmt.Println()
+			fmt.Println(err)
+			fmt.Println("\n" + separator)
+			continue
+		}
+
+		return page
+	}
+}
+
+func printPage(page Page) {
+	fmt.Println()
+	fmt.Println(separator)
+	fmt.Println()
+	fmt.Println("page", page.PageNumber)
+	fmt.Println()
+	fmt.Println(page.Text)
+
+	if len(page.Choices) == 0 {
+		return
+	}
+
+	fmt.Println(separator)
+	fmt.Println()
+	for _, choice := range page.Choices {
+		fmt.Println(choice)
+		fmt.Println()
+	}
+
 }
 
 func getBook() (*Book, error) {
 	book := &Book{}
-	source, err := ioutil.ReadFile("sample.yaml")
+	filename := os.Args[1]
+	fmt.Println(filename)
+	source, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return book, err
 	}
@@ -60,4 +137,8 @@ func getBook() (*Book, error) {
 	}
 
 	return book, nil
+}
+
+func isTerminalPage(page Page) bool {
+	return len(page.Choices) == 0
 }
